050631b19a32750f621b92fb6f4fd2eb5a1024c7
SBNAYAK <satya226@gmail.com>
2024-01-23 10:09:10 +0530

    Valid Pairs of Bracket

--- Logical Codings On Playground/ValidPairBracket.playground/Contents.swift
+++ Logical Codings On Playground/ValidPairBracket.playground/Contents.swift
@@ -4,28 +4,52 @@ class ValidBracket {
     var stack = StackArray()
     let validDict = ["{":"}", "(":")", "[":"]"]
     func inValid(_ S: String) -> Bool {
+       
+        
         if S.count <= 1{
+            
             return false
         }
-        
+        var i = 0
         for char in S {
+            print("\(char)  ---")
+        }
+        for char in S {
+            print("\(i) --- > \(char)")
             if (char == "{") || (char == "(") || (char == "["){
                 stack.push(String(char))
-            }else if(char == "}") || (char == ")") || (char == "]"){
+            }else if (char == "}") || (char == ")") || (char == "]"){
                 if let last = stack.peek(){
-                    
+                    if let validClosing = validDict[last] {
+                        if validClosing == String(char){
+                            stack.pop()
+                        }else{
+                            return false
+                        }
+                    }
+                }
+                else {
+                    stack.push(String(char))
                 }
             }
+            print("Done execution")
+            i = i + 1
         }
-        
+        if stack.isEmpty(){
+            return false
+        }
         
         return false
     }
 }
+print(ValidBracket().inValid("{}()[]"))
+
+
+
 struct StackArray {
-    private var items: [String] = []
+     var items: [String] = [String]()
     
-    func peek() -> String {
+    func peek() -> String? {
         guard let topElement = items.first else { fatalError("This stack is empty.") }
         return topElement
     }
@@ -37,4 +61,10 @@ struct StackArray {
     mutating func push(_ element: String) {
         items.insert(element, at: 0)
     }
+    func isEmpty() -> Bool{
+        return (items.last != nil) ? false : true
+    }
 }
+
+
+
